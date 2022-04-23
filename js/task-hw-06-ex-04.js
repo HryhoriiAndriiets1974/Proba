11384f8852e7d1b72040a200862ee11e228edd20
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-23 08:41:46 +0300

    +hw-06-ep-04

--- js/task-hw-06-ex-04.js
+++ js/task-hw-06-ex-04.js
@@ -4,11 +4,11 @@ const button = document.querySelectorAll("button");
 
 button[0].addEventListener("click", () => {
   counterValue.textContent -= 1;
-
 });
+
 button[1].addEventListener("click", () => {
   counterValue.textContent =
-  parseInt(document.querySelector("#value").textContent) + 1;
+  parseInt(counterValue.textContent) + 1;
 });
 // ---------------------- 2 new
 // const counterValue = {
